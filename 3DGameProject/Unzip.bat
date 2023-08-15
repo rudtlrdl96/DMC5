6e9cd13d436daf2471ed9f540a6ddbfbbfec8086
엄태건 <umtimos@naver.com>
2023-08-16 03:54:47 +0900

    엄태건 : bat 파일 수정

--- 3DGameProject/Unzip.bat
+++ 3DGameProject/Unzip.bat
@@ -17,12 +17,3 @@ IF NOT EXIST "%~dp0%GameEngineCore\ThirdParty\FBX\lib\x64\Release\libfbxsdk-md.l
 IF NOT EXIST "%~dp0%GameEngineCore\ThirdParty\FBX\lib\x64\Release\libfbxsdk-mt.lib" (
     powershell expand-archive GameEngineCore\ThirdParty\FBX\lib\x64\Release\libfbxsdk-mt.zip GameEngineCore\ThirdParty\FBX\lib\x64\Release\
 )
-
-
-IF NOT EXIST "%~dp0%GameEngineCore\ThirdParty\PhysX_5\lib\x64\Debug\PhysXGpu_64.dll" (
-    powershell expand-archive GameEngineCore\ThirdParty\PhysX_5\lib\x64\Debug\PhysXGpu_64.zip GameEngineCore\ThirdParty\PhysX_5\lib\x64\Debug\
-)
-
-IF NOT EXIST "%~dp0%GameEngineCore\ThirdParty\PhysX_5\lib\x64\Release\PhysXGpu_64.dll" (
-    powershell expand-archive GameEngineCore\ThirdParty\PhysX_5\lib\x64\Release\PhysXGpu_64.zip GameEngineCore\ThirdParty\PhysX_5\lib\x64\Release\
-)
