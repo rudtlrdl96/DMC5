4c876479f0ca1a897eb6483b47609ecbb74ec42f
승민 황 <iqmin11@gmail.com>
2023-08-24 13:29:53 +0900

    황승민 : 수정

--- 3DGameProject/GameEngineContents/ContentsCore.cpp
+++ 3DGameProject/GameEngineContents/ContentsCore.cpp
@@ -43,7 +43,7 @@ void ContentsCore::GameStart()
 	//
 	GameEngineCore::CreateLevel<StageEditLevel>();
 
-	GameEngineCore::ChangeLevel("StartStage");
+	GameEngineCore::ChangeLevel("MainLevel");
 }
 
 void ContentsCore::GameEnd() 
