3ebd17e2ce2fa29da00670c3774f31e426c4982a
동민 유 <vpt515@gmail.com>
2023-08-24 14:02:16 +0900

    유동민 : Tool Base구조 설정중

--- 3DGameProject/GameEngineContents/StageEditGUI.cpp
+++ 3DGameProject/GameEngineContents/StageEditGUI.cpp
@@ -12,6 +12,7 @@ StageEditGUI::~StageEditGUI()
 {
 }
 
+
 void StageEditGUI::OnGUI(std::shared_ptr<GameEngineLevel> Level, float _DeltaTime)
 {
 	if (nullptr == Level)
@@ -29,9 +30,28 @@ void StageEditGUI::OnGUI(std::shared_ptr<GameEngineLevel> Level, float _DeltaTim
            PreChar[NumberIndex] = "stage" + to_string(NumberIndex);
            StageNumber.push_back(PreChar[NumberIndex].c_str());
         }
-        current = 0;
+        static int current = 0;
         ImGui::ListBox("StageDataBox", &current, StageNumber.data(), static_cast<int>(StageNumber.size()), EditBoxSize);
         ImGui::SameLine();
+        for (int i = 0; i < StageNumber.size(); ++i)
+        {
+            if (current == i && ImGui::Button("Delete"))
+            {
+                //delete
+            }
+            ImGui::SameLine();
+            if (current == i && ImGui::Button("Edit"))
+            {
+                EditSwitch = !EditSwitch;
+                /*ImGui::DragScalar("drag s8", ImGuiDataType_S8, &s8_v, drag_speed, drag_clamp ? &s8_zero : NULL, drag_clamp ? &s8_fifty : NULL);*/
+                /*P_StageData.MapDatas[i].MeshFileName*/
+            }
+            if (EditSwitch == true)
+            {
+                ImGui::SeparatorText("StageDataEdit");
+            }
+        }
+       
     }
 }
     
