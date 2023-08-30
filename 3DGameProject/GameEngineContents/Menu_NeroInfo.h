d0a3dc00ba096bccae42b138c2a8df98032188c6
Minseok Kim <mskim6800@naver.com>
2023-08-30 15:21:58 +0900

    김민석 : NeroInven 추가

--- 3DGameProject/GameEngineContents/Menu_NeroInfo.h
+++ 3DGameProject/GameEngineContents/Menu_NeroInfo.h
@@ -21,18 +21,24 @@ protected:
 	void Update(float _Delta) override;
 
 private:
-
+	void MoveInven(float _Delta);
 	std::shared_ptr<InfoUIRender> NeroInfoPtr = nullptr;
 	std::shared_ptr<InfoUIRender> NeroInvenPtr = nullptr;
 	std::shared_ptr<InvenUIButton> Arrow1Ptr = nullptr;
 	std::shared_ptr<InvenUIButton> Arrow2Ptr = nullptr;
 	std::shared_ptr<class GameEngineFontRenderer> FontRender = nullptr;
-
-
-	std::vector<int> Test;
 	int Index = 0;
 	int PrevIndex = 0;
 	bool InvenPlusValue = false;
 	bool InvenMinusValue = false;
+	bool ScendMove = false;
+	bool SetPosValue = false;
+	bool StartInven = false;
+	float4 LeftPos = { 270.0f,-230.0f,0.0f };
+	float4 CenterPos = { 600.0f,-230.0f,0.0f };
+	float4 RightPos = { 930.0f,-230.0f,0.0f };
+
+	float Speed = 0.0f;
+	float Ratio = 3.0f;
 };
 
