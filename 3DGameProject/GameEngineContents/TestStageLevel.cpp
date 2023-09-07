51cf3de7a0db14f30557d915d939799ca0967d2b
승민 황 <iqmin11@gmail.com>
2023-09-07 16:29:14 +0900

    황승민 : Location2 콜리전 맵 일단락

--- 3DGameProject/GameEngineContents/TestStageLevel.cpp
+++ 3DGameProject/GameEngineContents/TestStageLevel.cpp
@@ -70,6 +70,7 @@ void TestStageLevel::LevelChangeStart()
 
 	CreateStage(AllStageDatas[0]);
 
-	//std::shared_ptr<PlayerActor_Nero> Nero = CreateActor<PlayerActor_Nero>();
-	//Nero->SetWorldPosition({ 0, 100, 0 });
+	std::shared_ptr<PlayerActor_Nero> Nero = CreateActor<PlayerActor_Nero>();
+	Nero->SinglePlayLoad();
+	Nero->SetWorldPosition({ 0, 100, 0 });
 }
