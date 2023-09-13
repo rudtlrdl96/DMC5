76851bdde43d416fa627a9fde23913a62f912452
Minseok Kim <mskim6800@naver.com>
2023-09-13 21:02:11 +0900

    김민석 : 네로HPUI 포지션 수정및 FBX 수정

--- 3DGameProject/GameEngineContents/NeroHPUI.cpp
+++ 3DGameProject/GameEngineContents/NeroHPUI.cpp
@@ -29,34 +29,35 @@ void NeroHPUI::Start()
 		}
 	}
 
-	NeroUI_HPGlass = CreateComponent<UIFBXRenderer>();
-	NeroUI_HPGlass->SetFBXMesh("NeroHPUI.FBX", "FBX");
-	NeroUI_HPGlass->GetTransform()->SetLocalPosition({ -535.0f,275.0f,0.0f });
+	NeroUI_HPGlass = CreateComponent<UIFBXRenderer>(0);
+	NeroUI_HPGlass->SetFBXMesh("Nero_HPGlass.FBX", "FBX_Low");
+	NeroUI_HPGlass->GetTransform()->SetLocalPosition({ -535.0f,273.0f,172.0f });
 	NeroUI_HPGlass->GetTransform()->SetLocalRotation({ -90.0f,0.0f,0.0f });
 	NeroUI_HPGlass->GetTransform()->SetLocalScale({ 0.6f,0.6f,0.6f });
+	NeroUI_HPGlass->LightOff();
+	NeroUI_Hud1 = CreateComponent<UIFBXRenderer>(2);
+	NeroUI_Hud1->SetFBXMesh("NeroEx01.FBX", "FBX_Low");
+	NeroUI_Hud1->GetTransform()->SetLocalPosition({ -711.0f,303.0f,20.0f });
+	NeroUI_Hud1->GetTransform()->SetLocalRotation({ 30.0f,-90.0f,-30.0f });
+	NeroUI_Hud1->GetTransform()->SetLocalScale({ 0.7f,0.7f,0.7f });
+	NeroUI_Hud1->LightOff();
+	NeroUI_Hud2 = CreateComponent<UIFBXRenderer>(2);
+	NeroUI_Hud2->SetFBXMesh("NeroEx02.FBX", "FBX_Low");
+	NeroUI_Hud2->GetTransform()->SetLocalPosition({ -708.0f,334.0f,0.0f });
+	NeroUI_Hud2->GetTransform()->SetLocalRotation({ 30.0f,-90.0f,-30.0f });
+	NeroUI_Hud2->GetTransform()->SetLocalScale({ 0.7f,0.7f,0.7f });
+	NeroUI_Hud2->LightOff();
+	NeroUI_Hud3 = CreateComponent<UIFBXRenderer>(2);
+	NeroUI_Hud3->SetFBXMesh("NeroEx03.FBX", "FBX_Low");
+	NeroUI_Hud3->GetTransform()->SetLocalPosition({ -703.0f,365.0f,0.0f });
+	NeroUI_Hud3->GetTransform()->SetLocalRotation({ 30.0f,-90.0f,-30.0f });
+	NeroUI_Hud3->GetTransform()->SetLocalScale({ 0.7f,0.7f,0.7f });
+	NeroUI_Hud3->LightOff();
 
-	NeroUI_Hud1 = CreateComponent<UIFBXRenderer>();
-	NeroUI_Hud1->SetFBXMesh("NeroHud123.FBX", "FBX");
-	NeroUI_Hud1->GetTransform()->SetLocalPosition({ -702.0f,245.0f,-153.0f });
-	NeroUI_Hud1->GetTransform()->SetLocalRotation({ 29.0f,-102.0f,-170.0f });
-	NeroUI_Hud1->GetTransform()->SetLocalScale({ 0.9f,0.9f,0.9f });
-	
-	NeroUI_Hud2 = CreateComponent<UIFBXRenderer>();
-	NeroUI_Hud2->SetFBXMesh("NeroIden2.FBX", "FBX");
-	NeroUI_Hud2->GetTransform()->SetLocalPosition({ -698.0f,289.0f,-300.0f });
-	NeroUI_Hud2->GetTransform()->SetLocalRotation({ 25.0f,-87.0f,1.0f });
-	NeroUI_Hud2->GetTransform()->SetLocalScale({ 0.9f,0.9f,0.9f });
-	
-	NeroUI_Hud3 = CreateComponent<UIFBXRenderer>();
-	NeroUI_Hud3->SetFBXMesh("NeroIden3.FBX", "FBX");
-	NeroUI_Hud3->GetTransform()->SetLocalPosition({ -690.0f,340.0f,-300.0f });
-	NeroUI_Hud3->GetTransform()->SetLocalRotation({ 22.0f,-60.0f,10.0f });
-	NeroUI_Hud3->GetTransform()->SetLocalScale({ 0.9f,0.9f,0.9f });
-
-	std::shared_ptr<GameEngineUIRenderer> NeroUI_Back = CreateComponent<GameEngineUIRenderer>();
+	std::shared_ptr<GameEngineUIRenderer> NeroUI_Back = CreateComponent<GameEngineUIRenderer>(0);
 	NeroUI_Back->SetTexture("HudBack.png");
 	NeroUI_Back->GetTransform()->SetLocalScale({ 97.0f,131.0f,0.0f });
-	NeroUI_Back->GetTransform()->SetLocalPosition({-696.0f,337.0f,0.0f});
+	NeroUI_Back->GetTransform()->SetLocalPosition({-696.0f,334.0f,40.0f});
 }
 
 void NeroHPUI::Update(float _DeltaTime)
