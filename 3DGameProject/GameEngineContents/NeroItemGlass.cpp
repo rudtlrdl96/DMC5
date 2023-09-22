5e105b5d18e35d3fe198e96a61187c8b4c2a8b8c
Minseok Kim <mskim6800@naver.com>
2023-09-22 12:38:53 +0900

    김민석 : 릭 수정 및 팔 로테이션(1)

--- 3DGameProject/GameEngineContents/NeroItemGlass.cpp
+++ 3DGameProject/GameEngineContents/NeroItemGlass.cpp
@@ -6,7 +6,6 @@
 #include "UIFBXRenderer.h"
 #include "PlayerActor_Nero.h"
 #include <GameEngineCore/GameEngineFontRenderer.h>
-
 NeroItemGlass::NeroItemGlass()
 {
 }
@@ -34,18 +33,24 @@ void NeroItemGlass::Start()
 		}
 	}
 	NeroUI_ItemGlass = UIFBXActorBase::CreateUIFBX(NeroUI_ItemGlass, { 560.0f,-380.0f,172.0f }, { 0.6f,0.6f,0.6f }, { -90.0f,0.0f,0.0f }, "NeroItemGlass.FBX");
-
-	NeroUI_Overture = UIFBXActorBase::CreateUIFBX(NeroUI_ItemGlass, { 730.0f,-300.0f,50.0f }, {5.0f,5.0f,5.0f }, { 90.0f,0.0f,180.0f }, "OvertureArmUI.FBX", "FBX");
-	//NeroUI_Gerbera = UIFBXActorBase::CreateUIFBX(NeroUI_ItemGlass, { 730.0f,-300.0f,50.0f }, {4.0f,4.0f,4.0f }, { 150.0f,30.0f,210.0f }, "GerberaArmUI.FBX", "FBX");
-	//NeroUI_BusterArm = UIFBXActorBase::CreateUIFBX(NeroUI_ItemGlass, { 730.0f,-300.0f,50.0f }, { 3.0f,3.0f,3.0f }, { 210.0f,00.0f,240.0f }, "BusterArmUI.FBX", "FBX");
-
+	//0번째 로테이션
+	NeroUI_Overture0 = UIFBXActorBase::CreateUIFBX(NeroUI_Overture0, { 635.0f,-300.0f,50.0f }, { 3.0f,3.0f,3.0f }, { 180.0f,0.0f,150.0f }, "OvertureArmUI.FBX", "FBX");
+	NeroUI_Overture0->Off();
+	//1번째 로테이션
+	NeroUI_Overture = UIFBXActorBase::CreateUIFBX(NeroUI_Overture, { 730.0f,-300.0f,50.0f }, {5.0f,5.0f,5.0f }, { 90.0f,0.0f,180.0f }, "OvertureArmUI.FBX", "FBX");
+	//2번째 로테이션
+	NeroUI_Gerbera = UIFBXActorBase::CreateUIFBX(NeroUI_Gerbera, { 730.0f,-300.0f,50.0f }, {4.0f,4.0f,4.0f }, { 150.0f,30.0f,210.0f }, "GerberaArmUI.FBX", "FBX");
+	//3번째 로테이션
+	NeroUI_BusterArm = UIFBXActorBase::CreateUIFBX(NeroUI_BusterArm, { 730.0f,-300.0f,50.0f }, { 3.0f,3.0f,3.0f }, { 210.0f,00.0f,240.0f }, "BusterArmUI.FBX", "FBX");
+	ArmList = PlayerActor_Nero::GetBreakerListPointer();
 	SetItemText();
 }
 
 void NeroItemGlass::Update(float _DeltaTime)
 {
 	MaxCount->SetText(std::to_string(MaxItem));
-	CurCount->SetText(std::to_string(PlayerActor_Nero::GetBreakerListPointer()->size()));
+	CurCount->SetText(std::to_string(ArmList->size()));
+	CurDevilBreaker = ArmList->back();
 	switch (CurDevilBreaker)
 	{
 	case DevilBreaker::None:
@@ -63,55 +68,19 @@ void NeroItemGlass::Update(float _DeltaTime)
 	default:
 		break;
 	}
+	MoveItem(_DeltaTime);
 	//테스트용 코드
 	//데빌 브레이커를 주웠을 때 상황
 	// 1단계 0번 1번 2번 인덱스의 데빌브레이커가 스케일이 줄며 90도로 회전한다 
 	// 2단계 기울기를 조절한다.
 	// 3단계 기울기 + 크기를 키운다.
-	if (true == GameEngineInput::IsUp("UIDEBUGMODE"))
+	// 데빌브레이커가 추가가 되면 
+	if (true == GameEngineInput::IsPress("UI_Tab"))
 	{
+
 		first = true;
 	}
-	if (first == true)
-	{
-		Time += _DeltaTime;
-		//NeroUI_Overture->GetTransform()->SetLocalPosition({ });
-		NeroUI_Overture->GetTransform()->SetLocalPosition(float4::LerpClamp({ 730.0f,-300.0f,50.0f }, { 635.0f,-300.0f,50.0f }, Time * 2.0f));
-		NeroUI_Overture->GetTransform()->SetLocalScale(float4::LerpClamp({ 5.0f,5.0f,5.0f }, { 3.0f,3.0f,3.0f }, Time*2.0f));
-		NeroUI_Overture->GetTransform()->SetLocalRotation(float4::LerpClamp({ 90.0f,0.0f,180.0f }, { 180.0f,0.0f,180.0f }, Time*2.0f));
-		if (first == true && Time>0.5f)
-		{
-			secound = true;
-			Time = 0.0f;
-			first = false;
-		}
-		
-	}
-	if (secound == true)
-	{
-		Time += _DeltaTime;
-		NeroUI_Overture->GetTransform()->SetLocalRotation(float4::LerpClamp({ 180.0f,0.0f,180.0f }, { 180.0f,0.0f,210.0f }, Time * 2.0f));
-		if (Time >= 0.8f)
-		{
-			secound = false;
-			third = true;
-			Time = 0.0f;
-
-		}
-	}
-	if (third == true)
-	{
-		Time += _DeltaTime;
-		NeroUI_Overture->GetTransform()->SetLocalPosition(float4::LerpClamp({ 635.0f, -300.0f, 50.0f}, { 730.0f,-300.0f,50.0f }, Time * 2.0f));
-		NeroUI_Overture->GetTransform()->SetLocalScale(float4::LerpClamp({ 3.0f,3.0f,3.0f }, { 4.0f,4.0f,4.0f }, Time * 2.0f));
-		NeroUI_Overture->GetTransform()->SetLocalRotation(float4::LerpClamp({ 180.0f,0.0f,210.0f }, { 150.0f,30.0f,210.0f }, Time * 2.0f));
-		if (Time >= 0.8f)
-		{
-			third = false;
-			Time = 0.0f;
-
-		}
-	}
+	
 }
 
 void NeroItemGlass::SetItemText()
@@ -146,5 +115,75 @@ void NeroItemGlass::SetItemText()
 	MaxCount->SetScale(36);
 	MaxCount->SetColor(float4(0.656f, 0.668f, 0.665f, 1.0f));
 	MaxCount->GetTransform()->SetLocalPosition({ 680.0f,-340.f,0.0f });
+
 }
 
+void NeroItemGlass::MoveItem(float _Delta)
+{
+	if (first == true)
+	{
+		Time += _Delta;
+		NeroUI_Overture0->Off();
+		NeroUI_Overture0->GetTransform()->SetLocalScale({ 3.0f,3.0f,3.0f });
+		NeroUI_Overture0->GetTransform()->SetLocalRotation({ 180.0f,0.0f,150.0f });
+		NeroUI_Overture0->GetTransform()->SetLocalPosition({ 635.0f,-300.0f,50.0f });
+
+		NeroUI_Overture->GetTransform()->SetLocalPosition(float4::LerpClamp({ 730.0f,-300.0f,50.0f }, { 635.0f,-300.0f,50.0f }, Time * 2.0f));
+		NeroUI_Overture->GetTransform()->SetLocalScale(float4::LerpClamp({ 5.0f,5.0f,5.0f }, { 3.0f,3.0f,3.0f }, Time * 2.0f));
+		NeroUI_Overture->GetTransform()->SetLocalRotation(float4::LerpClamp({ 90.0f,0.0f,180.0f }, { 180.0f,0.0f,180.0f }, Time * 2.0f));
+
+		NeroUI_Gerbera->GetTransform()->SetLocalPosition(float4::LerpClamp({ 730.0f,-300.0f,50.0f }, { 635.0f,-300.0f,50.0f }, Time * 2.0f));
+		NeroUI_Gerbera->GetTransform()->SetLocalScale(float4::LerpClamp({ 4.0f,4.0f,4.0f }, { 3.0f,3.0f,3.0f }, Time * 2.0f));
+		NeroUI_Gerbera->GetTransform()->SetLocalRotation(float4::LerpClamp({ 150.0f,30.0f,210.0f }, { 180.0f,30.0f,210.0f }, Time * 2.0f));
+
+		NeroUI_BusterArm->GetTransform()->SetLocalPosition(float4::LerpClamp({ 730.0f,-300.0f,50.0f }, { 730.0f,-300.0f,50.0f }, Time * 2.0f));
+		NeroUI_BusterArm->GetTransform()->SetLocalScale({ 3.0f,3.0f,3.0f });
+		NeroUI_BusterArm->GetTransform()->SetLocalRotation(float4::LerpClamp({ 210.0f,00.0f,240.0f }, { 210.0f,00.0f,300.0f }, Time * 2.0f));
+		
+		if (first == true && Time > 0.5f)
+		{
+			secound = true;
+			Time = 0.0f;
+			first = false;
+		}
+
+	}
+	if (secound == true)
+	{
+		Time += _Delta;
+		NeroUI_Overture0->On();
+		NeroUI_Overture0->GetTransform()->SetLocalRotation(float4::LerpClamp({ 180.0f,0.0f,150.0f }, { 180.0f,0.0f,180.0f }, Time * 2.0f));
+		NeroUI_Overture->GetTransform()->SetLocalRotation(float4::LerpClamp({ 180.0f,0.0f,180.0f }, { 180.0f,0.0f,210.0f }, Time * 2.0f));
+		NeroUI_Gerbera->GetTransform()->SetLocalRotation(float4::LerpClamp({ 180.0f,30.0f,210.0f }, { 180.0f,00.0f,240.0f }, Time * 2.0f));
+
+		if (Time >= 0.8f)
+		{
+			secound = false;
+			third = true;
+			Time = 0.0f;
+
+		}
+	}
+	if (third == true)
+	{
+		
+		Time += _Delta;
+		NeroUI_Overture0->GetTransform()->SetLocalPosition(float4::LerpClamp({ 635.0f, -300.0f, 50.0f }, { 730.0f,-300.0f,50.0f }, Time * 2.0f));
+		NeroUI_Overture0->GetTransform()->SetLocalScale(float4::LerpClamp({ 3.0f,3.0f,3.0f }, { 5.0f,5.0f,5.0f }, Time * 2.0f));
+		NeroUI_Overture0->GetTransform()->SetLocalRotation(float4::LerpClamp({180.0f,0.0f,180.0f }, { 90.0f,0.0f,180.0f }, Time * 2.0f));
+
+		NeroUI_Overture->GetTransform()->SetLocalPosition(float4::LerpClamp({ 635.0f, -300.0f, 50.0f }, { 730.0f,-300.0f,50.0f }, Time * 2.0f));
+		NeroUI_Overture->GetTransform()->SetLocalScale(float4::LerpClamp({ 3.0f,3.0f,3.0f }, { 4.0f,4.0f,4.0f }, Time * 2.0f));
+		NeroUI_Overture->GetTransform()->SetLocalRotation(float4::LerpClamp({ 180.0f,0.0f,210.0f }, { 150.0f,30.0f,210.0f }, Time * 2.0f));
+
+		NeroUI_Gerbera->GetTransform()->SetLocalPosition(float4::LerpClamp({ 635.0f,-300.0f,50.0f }, { 730.0f,-300.0f,50.0f }, Time * 2.0f));
+		NeroUI_Gerbera->GetTransform()->SetLocalScale(float4::LerpClamp({ 3.0f,3.0f,3.0f }, { 3.0f,3.0f,3.0f }, Time * 2.0f));
+		NeroUI_Gerbera->GetTransform()->SetLocalRotation(float4::LerpClamp({ 180.0f,00.0f,240.0f }, { 210.0f,00.0f,240.0f }, Time * 2.0f));
+		if (Time >= 0.8f)
+		{
+			third = false;
+			Time = 0.0f;
+
+		}
+	}
+}
--- 3DGameProject/GameEngineContents/NeroItemGlass.h
+++ 3DGameProject/GameEngineContents/NeroItemGlass.h
@@ -1,7 +1,16 @@
 #pragma once
 #include "UIFBXActorBase.h"
 #include "ContentsEnum.h"
-
+#include <GameEngineCore/GameEngineFSM.h>
+enum FSM_UIMOVEITEM_STATE
+{
+	UI_GetArm_Reduce,
+	UI_GetArm_Turn,
+	UI_GetArm_Grow,
+	UI_BrokeArm_Reduce,
+	UI_BrokeArm_Turn,
+	UI_BrokeArm_Grow
+};
 // Ό³Έν :
 class NeroItemGlass : public UIFBXActorBase
 {
@@ -24,8 +33,12 @@ protected:
 
 private:
 	void SetItemText();
+	void MoveItem(float _Delta);
+	GameEngineFSM FSM;
+
 	std::shared_ptr<class UIFBXRenderer> NeroUI_ItemGlass = nullptr;
 
+	std::shared_ptr<class UIFBXRenderer> NeroUI_Overture0 = nullptr;
 	std::shared_ptr<class UIFBXRenderer> NeroUI_Overture = nullptr;
 	std::shared_ptr<class UIFBXRenderer> NeroUI_Gerbera = nullptr;
 	std::shared_ptr<class UIFBXRenderer> NeroUI_BusterArm = nullptr;
@@ -35,11 +48,15 @@ private:
 	std::shared_ptr<class GameEngineFontRenderer> SlashText = nullptr;
 	std::shared_ptr<class GameEngineFontRenderer> MaxCount = nullptr;
 	DevilBreaker CurDevilBreaker = DevilBreaker::None;
+	DevilBreaker RecentDevilBreaker = DevilBreaker::None;
 
+	std::list<DevilBreaker>* ArmList;
 	int MaxItem = 4;
 	float Time = 0.0f;
 	bool first = false;
 	bool secound = false;
 	bool third = false;
+
+	int i = 0;
 };
 
