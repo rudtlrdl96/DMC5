d046f28a38db2f6d3ce1a52e820736909a82d235
Minseok Kim <mskim6800@naver.com>
2023-09-27 13:10:56 +0900

    김민석 : HP받아오는 함수 추가

--- 3DGameProject/GameEngineContents/UI_HPGaege.cpp
+++ 3DGameProject/GameEngineContents/UI_HPGaege.cpp
@@ -28,18 +28,18 @@ void UI_HPGaege::Start()
 	NeroHpAni->SetTexture("HPGaugeAni.png");
 	NeroHpAni->CreateAnimation({ .AnimationName = "HPANI", .SpriteName = "HPGaugeAni.png", .Start = 0, .End = 7, .Loop = true, .FrameTime = {.1f, .1f,.1f, .1f,.1f, .1f,.1f, .1f} });
 	NeroHpAni->GetTransform()->SetLocalScale({ 50.0f,170.0f,0.0f });
-	NeroHpAni->GetTransform()->SetLocalPosition({ -240.0f,940.0f,50.0f });
+	NeroHpAni->GetTransform()->SetLocalPosition({ -240.0f,944.0f,50.0f });
 	NeroHpAni->GetTransform()->SetLocalRotation({ 0.0f,0.0f,90.0f });
 	NeroHpAni->ChangeAnimation("HPANI");
 
 	std::shared_ptr<GameEngineUIRenderer> NeroHPUI_Front = CreateComponent<GameEngineUIRenderer>(0);
 	NeroHPUI_Front->SetTexture("hpbar.png");
-	NeroHPUI_Front->GetTransform()->SetLocalScale({ 492.0f,7.0f,0.0f });
-	NeroHPUI_Front->GetTransform()->SetLocalPosition({ -350.0f,404.3f,40.0f });
+	NeroHPUI_Front->GetTransform()->SetLocalScale({ 492.0f,8.0f,0.0f });
+	NeroHPUI_Front->GetTransform()->SetLocalPosition({ -350.0f,404.8f,40.0f });
 	std::shared_ptr<GameEngineUIRenderer> NeroHPUI_Back = CreateComponent<GameEngineUIRenderer>(0);
 	NeroHPUI_Back->SetTexture("hpHitbar.png");
-	NeroHPUI_Back->GetTransform()->SetLocalScale({ 492.0f,7.0f,0.0f });
-	NeroHPUI_Back->GetTransform()->SetLocalPosition({ -350.0f,404.3f,50.0f });
+	NeroHPUI_Back->GetTransform()->SetLocalScale({ 492.0f,8.0f,0.0f });
+	NeroHPUI_Back->GetTransform()->SetLocalPosition({ -350.0f,404.8f,50.0f });
 	for (int i = 0; i < 10; i++)
 	{
 		UIFBXActorBase::CreateGaugeBar({ -570.0f + i * 49.0f , 405.0f , 0.0f },{ 0.7f,0.6f,0.6f }, { 90.0f,0.0f,0.0f }, "HpGauge.FBX");
