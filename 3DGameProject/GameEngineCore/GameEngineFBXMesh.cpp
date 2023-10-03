4f0f1ad4d3092de8e6171d04d00af35b58bdfebd
DESKTOP-QCG6R4C\rudtl <rudtlrdl96@gmail.com>
2023-10-03 18:19:04 +0900

    김경식 : Tangent Binormal 생성코드 추가

--- 3DGameProject/GameEngineCore/GameEngineFBXMesh.cpp
+++ 3DGameProject/GameEngineCore/GameEngineFBXMesh.cpp
@@ -288,8 +288,12 @@ void GameEngineFBXMesh::LoadBinormal(fbxsdk::FbxMesh* _Mesh, fbxsdk::FbxAMatrix 
 
 	if (0 == iCount)
 	{
-		return;
+		if (float4::ZERO == _ArrVtx[_Index].TANGENT)
+		{
+			return;
+		}
 
+		_ArrVtx[_Index].BINORMAL = float4::Cross3DReturnNormal(_ArrVtx[_Index].NORMAL, _ArrVtx[_Index].TANGENT);
 	}
 
 	FbxGeometryElementBinormal* pElement = _Mesh->GetElementBinormal();
@@ -340,7 +344,12 @@ void GameEngineFBXMesh::LoadTangent(fbxsdk::FbxMesh* _Mesh, fbxsdk::FbxAMatrix _
 
 	if (0 == iCount)
 	{
-		return;
+		bool result = _Mesh->GenerateTangentsData(VtxId);
+		
+		if (false == result)
+		{
+			return;
+		}
 	}
 	FbxGeometryElementTangent* pElement = _Mesh->GetElementTangent();
 	int iDataIndex = VtxId;
