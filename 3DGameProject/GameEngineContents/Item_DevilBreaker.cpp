f46fc7b266dd4447d6d5cb06bfcb54cf364dbf95
김경학 <asd315406@gmail.com>
2023-10-11 17:47:54 +0900

    김경학 : 네로 아이템먹는거 구현

--- 3DGameProject/GameEngineContents/Item_DevilBreaker.cpp
+++ 3DGameProject/GameEngineContents/Item_DevilBreaker.cpp
@@ -14,26 +14,30 @@ Item_DevilBreaker::~Item_DevilBreaker()
 void Item_DevilBreaker::SetDevilBreaker(DevilBreaker _Value)
 {
 	Type = _Value;
-	ArmRenderer = CreateComponent<GameEngineFBXRenderer>();
-	//IconRenderer = CreateComponent<EffectRenderer>();
-	//IconRenderer->RectInit();
+	IconRenderer->RectInit("Effect_2D");
+	IconRenderer->LockRotation();
 	switch (_Value)
 	{
 	case DevilBreaker::None:
 		break;
 	case DevilBreaker::Overture:
 		ArmRenderer->SetFBXMesh("OvertureArmUI.FBX", "FBX");
-		
+		IconRenderer->SetTexture("DiffuseTexture", "ItemExPlane_OverTure.png");
 		break;
 	case DevilBreaker::Gerbera:
 		ArmRenderer->SetFBXMesh("GerberaArmUI.FBX", "FBX");
+		IconRenderer->SetTexture("DiffuseTexture", "ItemExPlane_Gerbera.png");
 		break;
 	case DevilBreaker::BusterArm:
 		ArmRenderer->SetFBXMesh("BusterArmUI.FBX", "FBX");
+		IconRenderer->SetTexture("DiffuseTexture", "ItemExPlane_BusterArm.png");
 		break;
 	default:
 		break;
 	}
+	IconRenderer->GetTransform()->SetWorldScale({ 268, 268, 268 });
+
+
 }
 
 DevilBreaker Item_DevilBreaker::Take()
@@ -44,21 +48,29 @@ DevilBreaker Item_DevilBreaker::Take()
 
 void Item_DevilBreaker::Start()
 {
-	GameEngineDirectory NewDir;
-	NewDir.MoveParentToDirectory("ContentResources");
-	NewDir.Move("ContentResources");
-	NewDir.Move("Mesh");
-	NewDir.Move("UIMesh");
-	NewDir.Move("NeroItemMesh");
-
-	if (nullptr == GameEngineFBXMesh::Find("NeroItemGlass.FBX"))
 	{
-		std::vector<GameEngineFile> Files = NewDir.GetAllFile({ ".fbx" });
-		for (GameEngineFile File : Files)
+		GameEngineDirectory NewDir;
+		NewDir.MoveParentToDirectory("ContentResources");
+		NewDir.Move("ContentResources");
+		NewDir.Move("Mesh");
+		NewDir.Move("UIMesh");
+		NewDir.Move("NeroItemMesh");
+
+		if (nullptr == GameEngineFBXMesh::Find("NeroItemGlass.FBX"))
 		{
-			GameEngineFBXMesh::Load(File.GetFullPath());
+			std::vector<GameEngineFile> Files = NewDir.GetAllFile({ ".fbx" });
+			for (GameEngineFile File : Files)
+			{
+				GameEngineFBXMesh::Load(File.GetFullPath());
+			}
 		}
 	}
+
+	ArmRenderer = CreateComponent<GameEngineFBXRenderer>();
+	IconRenderer = CreateComponent<EffectRenderer>();
+	IconRenderer->GetTransform()->SetLocalPosition({ 0, 256 });
+	IconRenderer->GetTransform()->SetWorldScale({ 268, 268, 268 });
+	IconRenderer->GetTransform()->SetWorldRotation(float4::ZERO);
 
 	Col = CreateComponent<GameEngineCollision>(CollisionOrder::DevilBreaker);
 	Col->GetTransform()->SetLocalScale({ 150, 150, 150 });
