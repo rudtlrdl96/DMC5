976a0de47c19c24af924ebf62a847a6a98ad5f77
엄태건 <umtimos@naver.com>
2023-10-13 16:02:56 +0900

    엄태건 : 작업하다 치매와서 주석정리 한번 조짐

--- 3DGameProject/GameEngineContents/BaseEnemyActor.cpp
+++ 3DGameProject/GameEngineContents/BaseEnemyActor.cpp
@@ -115,6 +115,10 @@ void BaseEnemyActor::Update(float _DeltaTime)
 	}
 }
 
+/// <summary>
+/// 액터 위치에서 아래 방향으로 Raycast를 실시하여 바닥(피직스 충돌체)을 체크한다.
+/// </summary>
+/// <param name="_Distance">Raycasting 길이, 몬스터마다 다름</param>
 bool BaseEnemyActor::FloorCheck(float _Distance)
 {
 	float4 StartPosision = this->GetTransform()->GetWorldPosition();
@@ -127,6 +131,9 @@ bool BaseEnemyActor::FloorCheck(float _Distance)
 	return IsResults;
 }
 
+/// <summary>
+/// 몬스터 최초 생성 시 Player 정보를 class BasePlayerActor* Player에 저장
+/// </summary>
 void BaseEnemyActor::PlayerCheckInit()
 {
 	std::vector<BasePlayerActor*>& Players = BasePlayerActor::GetPlayers();
@@ -147,6 +154,11 @@ void BaseEnemyActor::PlayerCheckInit()
 	}
 }
 
+/// <summary>
+/// RN_MonsterCollision에 충돌한 충돌체(Player) 정보를 class BasePlayerActor* Player에 저장
+/// </summary>
+/// <param name="_DeltaTime">시간</param>
+/// <param name="_Collision">체크할 충돌체</param>
 void BaseEnemyActor::PlayerContactCheck(float _DeltaTime, GameEngineCollision* _Collision)
 {
 	ContactDelayTime += _DeltaTime;
@@ -197,6 +209,10 @@ void BaseEnemyActor::PlayerContactCheck(float _DeltaTime, GameEngineCollision* _
 	}
 }
 
+/// <summary>
+/// 자신을 공격한 충돌체(Player) 정보를 class BasePlayerActor* Player에 저장
+/// </summary>
+/// <param name="_Collision">체크할 충돌체</param>
 void BaseEnemyActor::PlayerAttackCheck(GameEngineCollision* _Collision)
 {
 	GameEngineCollision* CheckCollision = _Collision;
@@ -238,6 +254,9 @@ void BaseEnemyActor::PlayerAttackCheck(GameEngineCollision* _Collision)
 	}
 }
 
+/// <summary>
+/// Player에 대해 현재 몬스터의 위치를 외적합니다. (어디로 돌 것인지 결정하기 위함)
+/// </summary>
 float4 BaseEnemyActor::MonsterAndPlayerCross()
 {
 	if (nullptr == Player)
@@ -261,6 +280,9 @@ float4 BaseEnemyActor::MonsterAndPlayerCross()
 	return CrossResult;
 }
 
+/// <summary>
+/// Player에 대해 현재 몬스터의 위치를 내적합니다. (해당 방향으로 몇도를 회전해야 하는지 알기 위함)
+/// </summary>
 float BaseEnemyActor::MonsterAndPlayerDotProduct()
 {
 	if (nullptr == Player)
@@ -284,6 +306,9 @@ float BaseEnemyActor::MonsterAndPlayerDotProduct()
 	return DotProductResult;
 }
 
+/// <summary>
+/// Player에 대해 현재 몬스터의 위치를 내적합니다. Degree로 값을 반환해줍니다.
+/// </summary>
 float BaseEnemyActor::MonsterAndPlayerDotProductDegree()
 {
 	float RotationValue = 0.0f;
@@ -318,6 +343,9 @@ float BaseEnemyActor::MonsterAndPlayerDotProductDegree()
 	return RotationValue;
 }
 
+/// <summary>
+/// Player에 대해 현재 몬스터의 위치를 외적, 내적을 한 뒤 어떤 방향으로 회전해야하는지 EnemyRotation EnemyRotationValue에 값을 저장합니다.
+/// </summary>
 void BaseEnemyActor::RotationCheck()
 {
 	float4 CrossResult = MonsterAndPlayerCross();
@@ -372,6 +400,9 @@ void BaseEnemyActor::RotationCheck()
 	DotProductValue = DotProductResult;
 }
 
+/// <summary>
+/// Player에 대해 현재 몬스터의 위치를 외적, 내적을 한 뒤 어떤 방향에서 공격 받았는지를 EnemyHitDirect EnemyHitDirValue에 저장합니다.
+/// </summary>
 void BaseEnemyActor::AttackDirectCheck()
 {
 	float4 CrossResult = MonsterAndPlayerCross();
@@ -416,6 +447,9 @@ void BaseEnemyActor::AttackDirectCheck()
 	}
 }
 
+/// <summary>
+/// Slerp로 회전해야 하는 경우에 대한 값을 계산합니다. 계산 결과는 float4 CurRotation, float4 GoalRotation에 저장됩니다. Y축에 대한 회전만 따집니다.
+/// </summary>
 void BaseEnemyActor::SlerpCalculation()
 {
 	CurRotation = GetTransform()->GetWorldRotation();
@@ -441,61 +475,10 @@ void BaseEnemyActor::SlerpCalculation()
 	GoalRotation.z = 0.0f;
 }
 
-// 실패작 (아직 안씀)
-void BaseEnemyActor::AnimationSlerpCalculation()
-{
-	StartFrame = EnemyRenderer->GetCurFrame();
-	EndFrame = EnemyRenderer->GetEndFrame() -1;
-	CalFrame = static_cast<float>(EndFrame - StartFrame);
-	AnimationRotationTime = CalFrame * (1.f / 60.f);
-
-	CurRotation = GetTransform()->GetWorldRotation();
-
-	if (CurRotation.y <= 0.0f)
-	{
-		CurRotation.y += 360.f;
-	}
-
-	float4 Value = float4{ 0.0f, AnimationRotation, 0.0f };
-
-	GoalRotation = CurRotation + Value;
-
-	if (GoalRotation.y <= 0.0f)
-	{
-		CurRotation.y += 360.f;
-		GoalRotation = CurRotation + Value;
-	}
-
-	CurRotation.x = 0.0f;
-	CurRotation.z = 0.0f;
-	GoalRotation.x = 0.0f;
-	GoalRotation.z = 0.0f;
-}
-
-// 실패작 (아직 안씀)
-void BaseEnemyActor::AnimationSlerpTurn(float _DeltaTime)
-{
-	UINT Frmae = 0;
-
-	if (StartFrame <= 0)
-	{
-		return;
-	}
-
-	if (nullptr != EnemyRenderer)
-	{
-		Frmae = EnemyRenderer->GetCurFrame();
-	}
-
-	if (StartFrame < Frmae)
-	{
-		return;
-	}
-	
-	SlerpTime += _DeltaTime;
-	RotationValue = float4::SLerpQuaternion(CurRotation, GoalRotation, SlerpTime);
-}
-
+/// <summary>
+/// 일정 시간 동안 SlerpCalculation()()로 계산된  값을 향해 몬스터를 Slerp시킨다.
+/// </summary>
+/// <param name="_DeltaTime">시간</param>
 void BaseEnemyActor::SlerpTurn(float _DeltaTime)
 {
 	SlerpTime += _DeltaTime;
@@ -503,6 +486,9 @@ void BaseEnemyActor::SlerpTurn(float _DeltaTime)
 	PhysXCapsule->SetWorldRotation(RotationValue);
 }
 
+/// <summary>
+/// 몬스터의 현재 위치와 각도에 대해 전방, 후방, 좌측, 우측에 대한 노말 벡터값을 결정해준다. Phsysics와 Engine 연산이 1프레임 차이나기 때문에 벡터를 임의로 회전시킨다.
+/// </summary>
 void BaseEnemyActor::AllDirectSetting()
 {
 	PhysXCapsule->AddWorldRotation({ 0, DotProductValue, 0 });
@@ -519,6 +505,9 @@ void BaseEnemyActor::AllDirectSetting()
 	LeftDirect.RotationYDeg(DotProductValue);
 }
 
+/// <summary>
+/// 몬스터의 현재 위치와 각도에 대해 날아갈 방향을 PushDirect에 노말벡터로 저장한다.
+/// </summary>
 void BaseEnemyActor::PushDirectSetting()
 {
 	if (nullptr == Player)
@@ -529,6 +518,10 @@ void BaseEnemyActor::PushDirectSetting()
 	PushDirect = Player->GetTransform()->GetWorldForwardVector().NormalizeReturn();
 }
 
+/// <summary>
+/// 슈퍼아머 상태에서 히트 시 랜더러를 좌우로 흔든다.
+/// </summary>
+/// <param name="_DeltaTime">0.025초마다 1회 떨림 체크를 위한 시간</param>
 void BaseEnemyActor::RenderShake(float _DeltaTime)
 {
 	if (false == IsRenderShaking)
@@ -573,6 +566,10 @@ void BaseEnemyActor::RenderShake(float _DeltaTime)
 	}
 }
 
+/// <summary>
+/// 버스터 히트 시 몬스터를 Player FBX Bone의 어떤 위치에 부착할지 결정한다.
+/// </summary>
+/// <param name="_attachposition">FBX Bone에 부착된 상태에서의 랜더러 Position</param>
 void BaseEnemyActor::BusterCalculation(float4 _attachposition)
 {
 	if (nullptr == Player)
@@ -589,6 +586,9 @@ void BaseEnemyActor::BusterCalculation(float4 _attachposition)
 	Player->GetPlayerRenderer()->SetAttachTransform("R_Hand", EnemyRenderer->GetTransform(), _attachposition/*, float4(0.0f, 0.0f, 0.0f)*/);
 }
 
+/// <summary>
+/// 버스터 종료 시 몬스터 랜더러의 위치를 원래 위치로 되돌린다.
+/// </summary>
 void BaseEnemyActor::BusterEnd()
 {
 	if (nullptr == Player)
@@ -600,6 +600,9 @@ void BaseEnemyActor::BusterEnd()
 	EnemyRenderer->GetTransform()->SetLocalPosition(CurRenderPosition);
 }
 
+/// <summary>
+/// 스내치 히트 시 Lerp할 몬스터와 Player의 위치값을 float4 SnatchStartPosition, float4 SnatchEndPosition에 값을 저장한다.
+/// </summary>
 void BaseEnemyActor::SnatchCalculation()
 {
 	if (nullptr == Player)
@@ -612,6 +615,10 @@ void BaseEnemyActor::SnatchCalculation()
 	SnatchEndPosition = Player->GetTransform()->GetWorldPosition() + Forword + float4{0.f, 20.f, 0.f};
 }
 
+/// <summary>
+/// 일정 시간(1.0f) 동안 계산된 SnatchCalculation()로 계산된 값을 향해 몬스터를 Lerp시킨다.
+/// </summary>
+/// <param name="_DeltaTime">1.0초를 체크할 시간</param>
 void BaseEnemyActor::MonsterSnatch(float _DeltaTime)
 {
 	if (false == IsSnatch)
@@ -631,6 +638,10 @@ void BaseEnemyActor::MonsterSnatch(float _DeltaTime)
 	}
 }
 
+/// <summary>
+/// 입력된 DamageType에 따라 일정시간동안 타임스케일을 낮춥니다
+/// </summary>
+/// <param name="_Type">DamageType</param>
 void BaseEnemyActor::HitStop(DamageType _Type)
 {
 	float _TimeScale = 0.1f;
@@ -664,10 +675,14 @@ void BaseEnemyActor::HitStop(DamageType _Type)
 	});
 }
 
-void BaseEnemyActor::StopTime(float _Time)
+/// <summary>
+/// 일정시간동안 타임스케일을 0으로 합니다. _DeltaTime 초 후 타임스케일은 1이 됩니다. CapsuleCollision은 Off됩니다
+/// </summary>
+/// <param name="_DeltaTime">타임 스케일이 1이 되기까지의 시간을 체크하기 위함</param>
+void BaseEnemyActor::StopTime(float _DeltaTime)
 {
 	SetTimeScale(0.0f);
-	GetLevel()->TimeEvent.AddEvent(_Time, [=](GameEngineTimeEvent::TimeEvent _Event, GameEngineTimeEvent* _Manager)
+	GetLevel()->TimeEvent.AddEvent(_DeltaTime, [=](GameEngineTimeEvent::TimeEvent _Event, GameEngineTimeEvent* _Manager)
 	{
 		SetTimeScale(1.0f);
 	});
--- 3DGameProject/GameEngineContents/BaseEnemyActor.h
+++ 3DGameProject/GameEngineContents/BaseEnemyActor.h
@@ -77,13 +77,13 @@ protected:
 	void Start() override;
 	void Update(float _DeltaTime) override;
 
-	virtual void EnemyMeshLoad() = 0;
-	virtual void EnemyTypeLoad() = 0;
-	virtual void EnemyAnimationLoad() = 0;
-	virtual void EnemyCreateFSM() = 0;
-	virtual void EnemyCreateFSM_Client() = 0;
-	virtual void DamageCollisionCheck(float _DeltaTime) = 0;
-	virtual void DamageCollisionCheck_Client(float _DeltaTime) = 0;
+	virtual void EnemyTypeLoad() = 0;                                 // 기본 정보 입력 (HP, EnemyCode)
+	virtual void EnemyMeshLoad() = 0;                                 // 매쉬 로드, 애니메이션 전에 실시한다.
+	virtual void EnemyAnimationLoad() = 0;                            // 애니메이션 로드, 매쉬 로드 이후 실시한다.
+	virtual void EnemyCreateFSM() = 0;                                // 서버, 싱글용 FSM 생성
+	virtual void EnemyCreateFSM_Client() = 0;                         // 클라이언트용 FSM 생성
+	virtual void DamageCollisionCheck(float _DeltaTime) = 0;          // 서버, 싱글에서 처리할 데미지 콜리전 체크 함수
+	virtual void DamageCollisionCheck_Client(float _DeltaTime) = 0;   // 클라이언트에서 처리할 데미지 콜리전 체크 함수
 	virtual void RecognizeCollisionCheck(float _DeltaTime) = 0;
 
 	std::shared_ptr<class GameEngineFBXRenderer> EnemyRenderer = nullptr;     // 랜더러
@@ -102,9 +102,9 @@ protected:
 
 	//====================================================
 	// 플레이어 세팅
-	void PlayerCheckInit();
-	void PlayerContactCheck(float _DeltaTime, GameEngineCollision* _Collision);
-	void PlayerAttackCheck(GameEngineCollision* _Collision);
+	void PlayerCheckInit();                                                          // 몬스터 최초 생성 시 플레이어 정보를 class BasePlayerActor* Player에 저장
+	void PlayerContactCheck(float _DeltaTime, GameEngineCollision* _Collision);      // RN_MonsterCollision에 충돌한 충돌체(플레이어) 정보를 class BasePlayerActor* Player에 저장
+	void PlayerAttackCheck(GameEngineCollision* _Collision);                         // 자신을 공격한 충돌체(플레이어) 정보를 class BasePlayerActor* Player에 저장
 
 	class BasePlayerActor* Player = nullptr;
 	GameEngineFSM EnemyFSM;
@@ -114,8 +114,8 @@ protected:
 
 	//====================================================
 	// 몬스터 바닥 체크 (RayCast)
-	bool FloorCheck(float _Distance);
-	float FallDistance = 0.0f;     // FloorCheck() 시 필요, 각자의 몬스터 Start 부분에서 값 적용
+	bool FloorCheck(float _Distance);    // 액터 위치에서 아래 방향으로 Raycast를 실시하여 바닥(피직스 충돌체)을 체크한다.
+	float FallDistance = 0.0f;           // FloorCheck() 시 필요, 각자의 몬스터 Start 부분에서 값 적용
 	//====================================================
 
 	//====================================================
@@ -127,112 +127,127 @@ protected:
 	short ShakingCount = 0;
 	short ShakingMaxCount = 0;
 
-	void RenderShake(float _DeltaTime);
+	void RenderShake(float _DeltaTime);  // 슈퍼아머 상태에서 히트 시 랜더러를 좌우로 흔든다.
 	//====================================================
 
 	//====================================================
 	// Slerp 계산에 사용
-	EnemyRotation EnemyRotationValue = EnemyRotation::Forward;
-	EnemyHitDirect EnemyHitDirValue = EnemyHitDirect::Forward;
+	EnemyRotation EnemyRotationValue = EnemyRotation::Forward;  // 몬스터가 어떤 방향으로 회전해야 하는지에 대한 값
+	EnemyHitDirect EnemyHitDirValue = EnemyHitDirect::Forward;  //
 
-	float4 CurRotation = float4::ZERO;
-	float4 GoalRotation = float4::ZERO;
-	float4 RotationValue = float4::ZERO;     // Slerp 계산된 결과값
-	float SlerpTime = 0.0f;
-	float DotProductValue = 0.0f;            // CheckHeadingRotationValue() 실행 후 내적 결과값
-	float AnimationRotation = 0.0f;
-	float AnimationRotationTime = 0.0f;
-	float CalFrame = 0.0f;
-	UINT StartFrame = 0;
-	UINT EndFrame = 0;
-	bool AnimationTurnStart = false;
+	float4 CurRotation = float4::ZERO;                          // Slerp 시작 전 현재 각도
+	float4 GoalRotation = float4::ZERO;                         // Slerp 시작 시 목표로 하는 각도
+	float4 RotationValue = float4::ZERO;                        // Slerp실행 시 계산된 결과값(현재 각도)
+	float4 SnatchStartPosition = float4::ZERO;                  // Snatch 시작 전 현재 위치
+	float4 SnatchEndPosition = float4::ZERO;				    // Snatch 시작 시 목표로 하는 위치
+	float SnatchTime = 0.0f;								    // Snatch 시작 시 소요된 시간 (Lerp 시간)
+	float SlerpTime = 0.0f;									    // Slerp 시작 시 소요된 시간 (Slerp 시간)
+	float DotProductValue = 0.0f;                               // CheckHeadingRotationValue() 실행 후 내적 결과값
+	bool IsSnatch = false;									    // true로 변경 시 Snatch 실시, 끝나면 false로 변경됨
+	bool AnimationTurnStart = false;						    // true로 변경 시 랜더러가 턴 됐을 때 RotationCheck()를 실시할지 결정, 히트 시 false로 변경되며 몬스터를 해당 위치로 바로 회전시킴
 
-	float4 MonsterAndPlayerCross();			 // 플레이어와 몬스터 외적
-	float MonsterAndPlayerDotProduct();      // 플레이어와 몬스터 내적 (그냥 내적)
-	float MonsterAndPlayerDotProductDegree();// 플레이어와 몬스터 내적값 deg로 반환 (오른쪽 +, 왼쪽 -, 180도 까지)
-	void RotationCheck();            		 // 내적, 외적 후 어떤 식으로 회전할지 결정
-	void AttackDirectCheck();                // 플레이어 공격 방향 체크
-	void SlerpCalculation();				 // slerp 조건 계산
-	void SlerpTurn(float _DeltaTime);        // slerp로 턴
-	void AnimationSlerpCalculation();
-	void AnimationSlerpTurn(float _DeltaTime);
+	float4 MonsterAndPlayerCross();			         // Player에 대해 현재 몬스터의 위치를 외적합니다. (어디로 돌 것인지 결정하기 위함)
+	float MonsterAndPlayerDotProduct();              // Player에 대해 현재 몬스터의 위치를 내적합니다. (해당 방향으로 몇도를 회전해야 하는지 알기 위함)
+	float MonsterAndPlayerDotProductDegree();        // Player에 대해 현재 몬스터의 위치를 내적합니다. Degree로 값을 반환해줍니다.
+	void RotationCheck();            		         // Player에 대해 현재 몬스터의 위치를 외적, 내적을 한 뒤 어떤 방향으로 회전해야하는지를 EnemyRotation EnemyRotationValue에 저장합니다.
+	void AttackDirectCheck();                        // Player에 대해 현재 몬스터의 위치를 외적, 내적을 한 뒤 어떤 방향에서 공격 받았는지를 EnemyHitDirect EnemyHitDirValue에 저장합니다.
+	void SlerpCalculation();				         // Slerp로 회전해야 하는 경우에 대한 값을 계산합니다. 계산 결과는 float4 CurRotation, float4 GoalRotation에 저장됩니다. Y축에 대한 회전만 따집니다.
+	void SlerpTurn(float _DeltaTime);                // 일정 시간 동안 SlerpCalculation()()로 계산된  값을 향해 몬스터를 Slerp시킨다. 값이 실시간으로 float4 RotationValue에 저장된다.
+	void BusterCalculation(float4 _attachposition);  // 버스터 히트 시 몬스터를 Player FBX Bone의 어떤 위치에 부착할지 결정한다.
+	void BusterEnd();                                // 버스터 종료 시 몬스터 랜더러의 위치를 원래 위치로 되돌린다.
+	void SnatchCalculation();                        // 스내치 히트 시 Lerp할 몬스터와 Player의 위치값을 float4 SnatchStartPosition, float4 SnatchEndPosition에 값을 저장한다.
+	void MonsterSnatch(float _DeltaTime);            // 일정 시간(1.0f) 동안 SnatchCalculation()로 계산된 값을 향해 몬스터를 Lerp시킨다.
+	//====================================================
+
+	//====================================================
+	// TimeScale Action
+	void HitStop(DamageType _Type);  // 입력된 DamageType에 따라 일정시간동안 타임스케일을 낮춥니다
+	void StopTime(float _DeltaTime); // 일정시간동안 타임스케일을 0으로 합니다. _Time 초 후 타임스케일은 1이 됩니다. CapsuleCollision은 Off됩니다
 	//====================================================
 
 	//====================================================
 	// 방향 설정
-	float4 ForwardDirect = float4::ZERO;
-	float4 BackDirect = float4::ZERO;
-	float4 RightDirect = float4::ZERO;
-	float4 LeftDirect = float4::ZERO;
-	float4 PushDirect = float4::ZERO;
-	float4 SnatchStartPosition = float4::ZERO;
-	float4 SnatchEndPosition = float4::ZERO;
-	float SnatchTime = 0.0f;
-	bool IsSnatch = false;
+	float4 ForwardDirect = float4::ZERO;  // 몬스터의 전방
+	float4 BackDirect = float4::ZERO;     // 몬스터의 후방
+	float4 RightDirect = float4::ZERO;	  // 몬스터의 우측
+	float4 LeftDirect = float4::ZERO;	  // 몬스터의 좌측
+	float4 PushDirect = float4::ZERO;	  // 몬스터의 날아갈 방향(후방)
 
-	void AllDirectSetting();
-	void PushDirectSetting();
-	void SnatchCalculation();
-	void BusterCalculation(float4 _attachposition);
-	void BusterEnd();
-	void MonsterSnatch(float _DeltaTime);
+	void AllDirectSetting();              // 몬스터의 현재 위치와 각도에 대해 전방, 후방, 좌측, 우측에 대한 노말 벡터값을 결정해준다. Phsysics와 Engine 연산이 1프레임 차이나기 때문에 벡터를 임의로 회전시킨다.
+	void PushDirectSetting();             // 몬스터의 현재 위치와 각도에 대해 날아갈 방향을 PushDirect에 노말벡터로 저장한다.
 	
+	/// <summary>
+	/// 몬스터를 전방으로 addforce 실시
+	/// </summary>
 	void SetAdvance(float _Value)
 	{
 		PhysXCapsule->SetPush(ForwardDirect * _Value);
 	}
 
+	/// <summary>
+	/// 몬스터를 후방으로 addforce 실시 (BackDirect 사용), 걷거나 회전에 대해 뒤로 밀려날 때 사용
+	/// </summary>
 	void SetThrowback(float _Value)
 	{
 		PhysXCapsule->SetPush(BackDirect * _Value);
 	}
 
+	/// <summary>
+	/// 몬스터를 후방으로 addforce 실시 (PushDirect 사용), 히트 시 날리기는 해당 로직 사용
+	/// </summary>
 	void SetPush(float _Value)
 	{
 		SetMoveStop();
 		PhysXCapsule->SetPush(PushDirect * _Value);
 	}
 
+	/// <summary>
+	/// 공중에서 Y+ 방향으로 addforce 실시, 작용 전에 현재 물리(속도)값을 초기화시킨 후 적용
+	/// </summary>
 	void SetAir(float _Value)
 	{
 		PhysXCapsule->SetAirState(_Value);
 	}
 
+	/// <summary>
+	/// 몬스터 전방으로 이동
+	/// </summary>
 	void SetForwardMove(float _Value)
 	{
 		PhysXCapsule->SetMove(ForwardDirect * _Value);
 	}
 
+	/// <summary>
+	/// 몬스터 후방으로 이동
+	/// </summary>
 	void SetBackMove(float _Value)
 	{
 		PhysXCapsule->SetMove(BackDirect * _Value);
 	}
 
+	/// <summary>
+	/// 몬스터 우측으로 이동
+	/// </summary>
 	void SetRightMove(float _Value)
 	{
 		PhysXCapsule->SetMove(RightDirect * _Value);
 	}
 
+	/// <summary>
+	/// 몬스터 좌측으로 이동
+	/// </summary>
 	void SetLeftMove(float _Value)
 	{
 		PhysXCapsule->SetMove(LeftDirect * _Value);
 	}
 
+	/// <summary>
+	/// 모든 힘, 속도값을 0으로 만듦
+	/// </summary>
 	void SetMoveStop()
 	{
 		PhysXCapsule->SetLinearVelocityZero();
 	}
-	//====================================================
-	// TimeScale 
-	// 입력된 DamageType에 따라 일정시간동안 타임스케일을 낮춥니다
-	void HitStop(DamageType _Type);
-	// 일정시간동안 타임스케일을 0으로 합니다 _Time 초 후 타임스케일은 1이 됩니다. CapsuleCollision은 Off됩니다
-	void StopTime(float _Time);
-
-	//====================================================
-	
-
-	//====================================================
 
 private:
 
