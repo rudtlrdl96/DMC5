3e43b7f03f707bbf8040e67aeb289a5eea32724e
엄태건 <umtimos@naver.com>
2023-10-24 15:33:08 +0900

    엄태건 : 애니메이션 세이브

--- 3DGameProject/GameEngineContents/Enemy_ScudoAngelo.cpp
+++ 3DGameProject/GameEngineContents/Enemy_ScudoAngelo.cpp
@@ -133,7 +133,7 @@ void Enemy_ScudoAngelo::EnemyAnimationLoad()
 	NewDir.Move("Enemy");
 	NewDir.Move("Scudo");
 	NewDir.Move("Animation");
-	NewDir.Move("special");
+	NewDir.Move("move");
 
 	//AnimationEvent::LoadAll
 	//(
