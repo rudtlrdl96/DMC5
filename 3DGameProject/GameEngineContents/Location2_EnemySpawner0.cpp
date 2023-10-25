20ef2a5a7dfd9ac93bfef3b7549696462e33dc7f
김경학 <51849541+onegoon123@users.noreply.github.com>
2023-10-25 17:35:12 +0900

    김경학 : 엠푸사 웨이브 업데이트

--- 3DGameProject/GameEngineContents/Location2_EnemySpawner0.cpp
+++ 3DGameProject/GameEngineContents/Location2_EnemySpawner0.cpp
@@ -24,23 +24,30 @@ void Location2_EnemySpawner0::Start()
 			BGMPlayer::SetBattleBGM();
 			GameEngineLevel* Level = GetLevel();
 			Level->DynamicThis<StageBaseLevel>()->RedSealWallOn();
-			MonsterAliveCount = 3;
+			MonsterAliveCount = 2;
 			NetworkObjectBase::PushReservedDestroyCallback(Net_ActorType::Empusa, std::bind(&EnemySpawner::DestroyMonster, this));
 
 			if (false == NetworkManager::IsClient())
 			{
 				std::vector<float4> EnemyPos =
 				{
-					{ 1029, 55, 1337 }, { 91, 55, 82 }, { -1228, 55, 712 }
+					{ 494, 55, 1464 }, { -241, 82, 119 }
 				};
-				for (size_t i = 0; i < 3; ++i)
+				std::vector<float> EnemyRot = { -3, 12};
+				for (size_t i = 0; i < 2; ++i)
 				{
-					std::shared_ptr<Enemy_Empusa> Empusa = Poolable<Enemy_Empusa>::PopFromPool(Level, static_cast<int>(ActorOrder::Enemy));
-					Empusa->GetPhysXComponent()->SetWorldPosition(EnemyPos[i]);
-					Empusa->GetPhysXComponent()->SetLinearVelocityZero();
-					Empusa->GetTransform()->SetWorldPosition(EnemyPos[i]);
-					Empusa->GetPhysXComponent()->Off();
-					Empusa->PushDeathCallback(std::bind(&EnemySpawner::DestroyMonster, this));
+					Level->TimeEvent.AddEvent(i * 0.15f, [=](GameEngineTimeEvent::TimeEvent& Event, GameEngineTimeEvent* Manager)
+					{
+							std::shared_ptr<Enemy_Empusa> Empusa = Poolable<Enemy_Empusa>::PopFromPool(Level, static_cast<int>(ActorOrder::Enemy));
+							Empusa->GetPhysXComponent()->SetWorldPosition(EnemyPos[i]);
+							Empusa->GetPhysXComponent()->SetWorldRotation(float4::UP * EnemyRot[i]);
+							Empusa->GetTransform()->SetWorldPosition(EnemyPos[i]);
+							Empusa->GetTransform()->SetWorldRotation(float4::UP * EnemyRot[i]);
+							Empusa->GetPhysXComponent()->SetLinearVelocityZero();
+							Empusa->GetPhysXComponent()->Off();
+							Empusa->PushDeathCallback(std::bind(&EnemySpawner::DestroyMonster, this));
+					});
+					
 				}
 			}
 		};
@@ -48,22 +55,29 @@ void Location2_EnemySpawner0::Start()
 	MonsterWave_Events.resize(1);
 	MonsterWave_Events[0] = [this]()
 	{
-		MonsterAliveCount = 3;
+		MonsterAliveCount = 5;
 		if (false == NetworkManager::IsClient())
 		{
 			GameEngineLevel* Level = GetLevel();
 			std::vector<float4> EnemyPos =
 			{
-				{ 1029, 55, 1337 }, { 91, 55, 82 }, { -1228, 55, 712 }
+				{ -1469, 55, 1600 }, { 274, 82, 2461 }, { 888, 82, 1813 }, { -3, 82, 91 }, { -965, 82, 2174 }
 			};
-			for (size_t i = 0; i < 3; ++i)
+			std::vector<float> EnemyRot = { 120, -167, -127, 12, 120};
+
+			for (size_t i = 0; i < 5; ++i)
 			{
-				std::shared_ptr<Enemy_Empusa> Empusa = Poolable<Enemy_Empusa>::PopFromPool(Level, static_cast<int>(ActorOrder::Enemy));
-				Empusa->GetPhysXComponent()->SetWorldPosition(EnemyPos[i]);
-				Empusa->GetPhysXComponent()->SetLinearVelocityZero();
-				Empusa->GetTransform()->SetWorldPosition(EnemyPos[i]);
-				Empusa->GetPhysXComponent()->Off();
-				Empusa->PushDeathCallback(std::bind(&EnemySpawner::DestroyMonster, this));
+				Level->TimeEvent.AddEvent(i * 0.15f, [=](GameEngineTimeEvent::TimeEvent& Event, GameEngineTimeEvent* Manager)
+					{
+						std::shared_ptr<Enemy_Empusa> Empusa = Poolable<Enemy_Empusa>::PopFromPool(Level, static_cast<int>(ActorOrder::Enemy));
+						Empusa->GetPhysXComponent()->SetWorldPosition(EnemyPos[i]);
+						Empusa->GetPhysXComponent()->SetWorldRotation(float4::UP * EnemyRot[i]);
+						Empusa->GetTransform()->SetWorldPosition(EnemyPos[i]);
+						Empusa->GetTransform()->SetWorldRotation(float4::UP * EnemyRot[i]);
+						Empusa->GetPhysXComponent()->SetLinearVelocityZero();
+						Empusa->GetPhysXComponent()->Off();
+						Empusa->PushDeathCallback(std::bind(&EnemySpawner::DestroyMonster, this));
+					});
 			}
 		}
 	};
