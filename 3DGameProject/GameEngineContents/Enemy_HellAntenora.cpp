a3084c814b443a461ef930a3fdb502ea0e3bd7d9
엄태건 <umtimos@naver.com>
2023-11-01 20:41:44 +0900

    엄태건 : end 수정

--- 3DGameProject/GameEngineContents/Enemy_HellAntenora.cpp
+++ 3DGameProject/GameEngineContents/Enemy_HellAntenora.cpp
@@ -375,11 +375,6 @@ void Enemy_HellAntenora::Start()
 
 		IsChangeState = true;
 		});
-
-	BindNetObjEvent(3, [this](std::vector<NetworkObjectBase*> _Actors)
-		{
-			BusterEnd_Client();
-		});
 }
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -2333,10 +2328,6 @@ void Enemy_HellAntenora::EnemyCreateFSM()
 	},
 	.End = [=] {
 	BusterEnd();
-	if (true == NetworkManager::IsServer())
-	{
-		ExcuteNetObjEvent(3, NetObjEventPath::ActiveToPassive, { Player });
-	}
 	}
 		});
 	// 버스트 히트 루프
