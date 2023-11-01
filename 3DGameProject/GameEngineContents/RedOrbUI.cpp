408de79cae1728278935991bd79c6373c66dd339
Minseok Kim <mskim6800@naver.com>
2023-11-01 12:00:30 +0900

    김민석 : 돈 숫자 차곡차곡올라가기

--- 3DGameProject/GameEngineContents/RedOrbUI.cpp
+++ 3DGameProject/GameEngineContents/RedOrbUI.cpp
@@ -2,8 +2,8 @@
 #include "RedOrbUI.h"
 #include <GameEngineCore/GameEngineUIRenderer.h>
 #include <GameEngineCore/GameEngineFontRenderer.h>
-bool RedOrbUI::IsRedOrbAni = false;
-int RedOrbUI::RedOrbValue = 400;
+int RedOrbUI::PrevOrb = false;
+int RedOrbUI::RedOrbValue = 100;
 RedOrbUI::RedOrbUI() 
 {
 }
@@ -20,10 +20,6 @@ void RedOrbUI::Start()
 	RedOrbDir.Move("Texture");
 	RedOrbDir.Move("UI");
 	RedOrbDir.Move("PlayLevelUI");
-	if (nullptr == GameEngineSprite::Find("RedOrbNumAni.png"))
-	{
-		GameEngineSprite::LoadSheet(RedOrbDir.GetPlusFileName("RedOrbNumAni.png").GetFullPath(), 1, 11);
-	}
 	if (nullptr == GameEngineTexture::Find("RedOrb.png"))
 	{
 		GameEngineTexture::Load(RedOrbDir.GetPlusFileName("RedOrb.png").GetFullPath());
@@ -39,140 +35,25 @@ void RedOrbUI::Start()
 	RedOrbFont->SetColor(float4::WHITE);
 	RedOrbFont->SetFontFlag(FW1_TEXT_FLAG::FW1_LEFT);
 	RedOrbFont->GetTransform()->SetLocalPosition({ 582.0f,407.0f,-50.0f });
-	AniCreate();
 }
 
 void RedOrbUI::Update(float _Delta)
 {
-	RedOrbFont->SetText(std::to_string(RedOrbValue));
+	if (RedOrbNum != RedOrbValue)
+	{
+		AddTime += _Delta;
+		RedOrbNum = GameEngineMath::LerpLimit(static_cast<float>(PrevOrb), static_cast<float>(RedOrbValue), AddTime*1.5f);
+		RedOrbFont->SetText(std::to_string(static_cast<int>(RedOrbNum)));
+	}
+	else
+	{
+		AddTime = 0.0f;
+	}
 	if (GameEngineInput::IsUp("UI_OrbCheat"))
 	{
+		PrevOrb = RedOrbValue;
 		RedOrbValue += 5000;
-		IsRedOrbAni = true;
-	}
-	if (IsRedOrbAni == true)
-	{
-		IsOff = true;
-		
-	}
-	if (IsOff == true)
-	{
-		AniTime += _Delta;
-		RedOrbFont->Off();
-		if (AniTime > 0.33f)
-		{
-			AniTime = 0.0f;
-			RedOrbFont->On();
-			IsOff = false;
-		}
-	}
-	UpdateOrb(_Delta);
-
-}
-
-void RedOrbUI::AniCreate()
-{
-
-	Num_Ani1 = CreateComponent<GameEngineUIRenderer>(3);
-	Num_Ani1->SetTexture("NullTexture.png");
-	Num_Ani1->CreateAnimation({ .AnimationName = "NumAni1", .SpriteName = "RedOrbNumAni.png", .Start = 0, .End = 10,.FrameInter = 0.03f, .Loop = false });
-	Num_Ani1->GetTransform()->SetLocalScale(AniScale);
-	Num_Ani1->GetTransform()->SetLocalPosition({ 685.0f,375.0f,0.0f });
-
-	Num_Ani2 = CreateComponent<GameEngineUIRenderer>(3);
-	Num_Ani2->SetTexture("NullTexture.png");
-	Num_Ani2->CreateAnimation({ .AnimationName = "NumAni2", .SpriteName = "RedOrbNumAni.png", .Start = 0, .End = 10,.FrameInter = 0.03f, .Loop = false });
-	Num_Ani2->GetTransform()->SetLocalScale(AniScale);
-	Num_Ani2->GetTransform()->SetLocalPosition({ 666.0f,375.0f,0.0f });
-
-	Num_Ani3 = CreateComponent<GameEngineUIRenderer>(3);
-	Num_Ani3->SetTexture("NullTexture.png");
-	Num_Ani3->CreateAnimation({ .AnimationName = "NumAni3", .SpriteName = "RedOrbNumAni.png", .Start = 0, .End = 10,.FrameInter = 0.03f, .Loop = false });
-	Num_Ani3->GetTransform()->SetLocalScale(AniScale);
-	Num_Ani3->GetTransform()->SetLocalPosition({ 647.0f,375.0f,0.0f });
-
-	Num_Ani4 = CreateComponent<GameEngineUIRenderer>(3);
-	Num_Ani4->SetTexture("NullTexture.png");
-	Num_Ani4->CreateAnimation({ .AnimationName = "NumAni4", .SpriteName = "RedOrbNumAni.png", .Start = 0, .End = 10,.FrameInter = 0.03f, .Loop = false });
-	Num_Ani4->GetTransform()->SetLocalScale(AniScale);
-	Num_Ani4->GetTransform()->SetLocalPosition({ 626.0f,375.0f,0.0f });
-
-	Num_Ani5 = CreateComponent<GameEngineUIRenderer>(3);
-	Num_Ani5->SetTexture("NullTexture.png");
-	Num_Ani5->CreateAnimation({ .AnimationName = "NumAni5", .SpriteName = "RedOrbNumAni.png", .Start = 0, .End = 10,.FrameInter = 0.03f, .Loop = false });
-	Num_Ani5->GetTransform()->SetLocalScale(AniScale);
-	Num_Ani5->GetTransform()->SetLocalPosition({ 605.0f,375.0f,0.0f });
-
-	Num_Ani6 = CreateComponent<GameEngineUIRenderer>(3);
-	Num_Ani6->SetTexture("NullTexture.png");
-	Num_Ani6->CreateAnimation({ .AnimationName = "NumAni6", .SpriteName = "RedOrbNumAni.png", .Start = 0, .End = 10,.FrameInter = 0.03f, .Loop = false });
-	Num_Ani6->GetTransform()->SetLocalScale(AniScale);
-	Num_Ani6->GetTransform()->SetLocalPosition({ 587.0f,375.0f,0.0f });
-
-
-}
-
-void RedOrbUI::UpdateOrb(float _Delta)
-{
-	if (IsRedOrbAni == true)
-	{
-		if (RedOrbValue >= 100000)
-		{
-			AniTime += _Delta;
-			Num_Ani1->GetTransform()->SetLocalPosition({ 685.0f,375.0f,0.0f });
-			Num_Ani2->GetTransform()->SetLocalPosition({ 666.0f,375.0f,0.0f });
-			Num_Ani3->GetTransform()->SetLocalPosition({ 647.0f,375.0f,0.0f });
-			Num_Ani4->GetTransform()->SetLocalPosition({ 626.0f,375.0f,0.0f });
-			Num_Ani5->GetTransform()->SetLocalPosition({ 605.0f,375.0f,0.0f });
-			Num_Ani6->GetTransform()->SetLocalPosition({ 587.0f,375.0f,0.0f });
-			Num_Ani1->ChangeAnimation("NumAni1");
-			Num_Ani2->ChangeAnimation("NumAni2");
-			Num_Ani3->ChangeAnimation("NumAni3");
-			Num_Ani4->ChangeAnimation("NumAni4");
-			Num_Ani5->ChangeAnimation("NumAni5");
-			Num_Ani6->ChangeAnimation("NumAni6");
-			IsRedOrbAni = false;
-			
-		}
-		else if (RedOrbValue <= 100000 && RedOrbValue >= 10000)
-		{
-			Num_Ani1->GetTransform()->SetLocalPosition({ 675.0f,375.0f,0.0f });
-			Num_Ani2->GetTransform()->SetLocalPosition({ 654.0f,375.0f,0.0f });
-			Num_Ani3->GetTransform()->SetLocalPosition({ 634.0f,375.0f,0.0f });
-			Num_Ani4->GetTransform()->SetLocalPosition({ 614.0f,375.0f,0.0f });
-			Num_Ani5->GetTransform()->SetLocalPosition({ 594.0f,375.0f,0.0f });
-			Num_Ani1->ChangeAnimation("NumAni1");
-			Num_Ani2->ChangeAnimation("NumAni2");
-			Num_Ani3->ChangeAnimation("NumAni3");
-			Num_Ani4->ChangeAnimation("NumAni4");
-			Num_Ani5->ChangeAnimation("NumAni5");
-			IsRedOrbAni = false;
-		}
-		else if (RedOrbValue <= 10000 && RedOrbValue >= 1000)
-		{
-			Num_Ani1->GetTransform()->SetLocalPosition({ 657.0f,375.0f,0.0f });
-			Num_Ani2->GetTransform()->SetLocalPosition({ 635.0f,375.0f,0.0f });
-			Num_Ani3->GetTransform()->SetLocalPosition({ 617.0f,375.0f,0.0f });
-			Num_Ani4->GetTransform()->SetLocalPosition({ 595.0f,375.0f,0.0f });
-			Num_Ani1->ChangeAnimation("NumAni1");
-			Num_Ani2->ChangeAnimation("NumAni2");
-			Num_Ani3->ChangeAnimation("NumAni3");
-			Num_Ani4->ChangeAnimation("NumAni4");
-			IsRedOrbAni = false;
-
-		}
-		else if (RedOrbValue <= 1000 && RedOrbValue >= 100)
-		{
-			Num_Ani1->GetTransform()->SetLocalPosition({ 638.0f,375.0f,0.0f });
-			Num_Ani2->GetTransform()->SetLocalPosition({ 618.0f,375.0f,0.0f });
-			Num_Ani3->GetTransform()->SetLocalPosition({ 593.0f,375.0f,0.0f });
-			Num_Ani1->ChangeAnimation("NumAni1");
-			Num_Ani2->ChangeAnimation("NumAni2");
-			Num_Ani3->ChangeAnimation("NumAni3");
-			IsRedOrbAni = false;
-		}
 	}
 	
-
 }
 
--- 3DGameProject/GameEngineContents/RedOrbUI.h
+++ 3DGameProject/GameEngineContents/RedOrbUI.h
@@ -15,6 +15,7 @@ public:
 	RedOrbUI& operator=(RedOrbUI&& _Other) noexcept = delete;
 	static void AddRedOrb(int _Value)
 	{
+		PrevOrb = RedOrbValue;
 		RedOrbValue += _Value;
 	}
 	static int GetRedOrb()
@@ -23,28 +24,20 @@ public:
 	}
 	static void SubRedOrb(int _Value)
 	{
+		PrevOrb = RedOrbValue;
 		RedOrbValue -= _Value;
 	}
 protected:
 	void Start() override;
 	void Update(float _Delta) override;
 private:
-	void AniCreate();
-	void UpdateOrb(float _Delta);
+
 	std::shared_ptr<class GameEngineUIRenderer> RedOrbRender = nullptr;
 	std::shared_ptr<class GameEngineFontRenderer> RedOrbFont = nullptr;
-	std::shared_ptr<class GameEngineUIRenderer> Num_Ani1 = nullptr;
-	std::shared_ptr<class GameEngineUIRenderer> Num_Ani2 = nullptr;
-	std::shared_ptr<class GameEngineUIRenderer> Num_Ani3 = nullptr;
-	std::shared_ptr<class GameEngineUIRenderer> Num_Ani4 = nullptr;
-	std::shared_ptr<class GameEngineUIRenderer> Num_Ani5 = nullptr;
-	std::shared_ptr<class GameEngineUIRenderer> Num_Ani6 = nullptr;
 	float4 Scale = { 43.0f,60.0f,0.0f };
-	float4 AniScale = { 33.0f,60.0f,0.0f };
-
+	static int PrevOrb;
 	static int RedOrbValue;
-	static bool IsRedOrbAni;
-	bool IsOff = false;
-	float AniTime = 0.0f;
+	float AddTime = 0.0f;
+	float RedOrbNum = 0.0f;
 };
 
